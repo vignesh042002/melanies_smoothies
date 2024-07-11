d66b57142782ad9e4766d3c8078fa36814590fd2
vignesh042002 <105711709+vignesh042002@users.noreply.github.com>
2024-07-11 09:36:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,6 @@ if ingredients_list:
 
 cnx=st.connection("snowflake")
 session=cnx.session()
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
 
 
 
