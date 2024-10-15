f1d433c8901800f0022711e156b238a760d2d5a3
vignesh042002 <105711709+vignesh042002@users.noreply.github.com>
2024-10-15 14:49:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,14 +33,14 @@ ingredients_list = st.multiselect(
 )
 if ingredients_list:
  ingredients_string = ''
- for fruit_chosen in ingredients_list:
-      ingredients_string +=fruit_chosen + ' '
-      search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+     for fruit_chosen in ingredients_list:
+          ingredients_string +=fruit_chosen + ' '
+          search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
       #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-      st.subheader(fruit_chosen + ' Nutrition Information')
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-      fv_df = st.dataframe(data= fruityvice_response.json(),use_container_width=True)
- #st.write(ingredients_string)
+          st.subheader(fruit_chosen + ' Nutrition Information')
+          fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+          fv_df = st.dataframe(data= fruityvice_response.json(),use_container_width=True)
+     #st.write(ingredients_string)
 
  my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order +"""')"""
